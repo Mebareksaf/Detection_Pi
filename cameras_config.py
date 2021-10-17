33e7cd50d30d605887879c0479a9d26a1560f1b1
Mebarek Saf <30500856+Mebareksaf@users.noreply.github.com>
2021-10-18 00:00:57 +0200

    Create cameras_config.py

---
+++ cameras_config.py
@@ -0,0 +1,6 @@
+cameras_config = [
+        "rtsp://username:password@X.X.X.X:554/cam/realmonitor?channel=1&subtype=1", #&proto=Onvif",
+        #"rtsp://username:password@X.X.X.X:554/cam/realmonitor?channel=1&subtype=2"#,
+        #"rtsp://username:password@X.X.X.X:554/cam/realmonitor?channel=1&subtype=1"
+
+        ]
